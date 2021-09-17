5c963fe1126558e54dcaaf20397e528f514a3606
Adam L <luzsiadam@gmail.com>
2021-09-17 22:05:53 +0200

    +  Lesson 2a / Updates 2

--- public/index.php
+++ public/index.php
@@ -9,8 +9,10 @@ $section = $_GET['section'] ?? 'home';
 
 if ($section=='about-us') {
     include 'controller/aboutUsPage.php';
-} else if ($section == 'contact'){
+} else if ($section == 'contact') {
     include 'controller/contactPage.php';
+} else {
+    include 'controller/homePage.php';
 }
 
 

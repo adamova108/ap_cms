2107b95c1454a47916cea3e58729b42464084a88
Adam L <luzsiadam@gmail.com>
2021-09-17 22:12:39 +0200

    +  Lesson 2c

--- public/index.php
+++ public/index.php
@@ -15,6 +15,39 @@ if ($section=='about-us') {
     include 'controller/homePage.php';
 }
 
+/*
+
+require_once 'src/controller.php';
+
+
+// if / else logic
+
+$section = $_GET['section'] ?? $_POST['section'] ?? 'home';
+$action = $_GET['action'] ?? $_POST['action'] ?? 'default';
+
+
+
+if ($section=='about-us') {
+
+    include 'controller/aboutUsPage.php';
+
+    $aboutController = new AboutUsController();
+    $aboutController->runAction($action);
+
+} else if ($section == 'contact'){
+
+    include 'controller/contactPage.php';
+    $contactController = new ContactController();
+    $contactController->runAction($action);
+
+} else {
+    include 'controller/homePage.php';
+}
+
+
+
+
+
 
 
 

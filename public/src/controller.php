6988c9a3e03b80c8142c68f953e8dcf68ab9fba3
Adam L <luzsiadam@gmail.com>
2021-09-27 21:26:06 +0200

    + Updates 5a

--- public/src/controller.php
+++ public/src/controller.php
@@ -2,24 +2,22 @@
 
 class Controller
 {
-    //put your code here
 
-    function runAction($actionName)
-    {
+	public function runAction($actionName)
+	{
+		if (method_exists($this, 'runBeforeAction')) {
+			$result = $this->runBeforeAction();
+			if ($result == false) {
+				return;
+			}
+		}
 
-        if (method_exists($this, 'runBeforeAction')) {
-            $result = $this->runBeforeAction();
-            if ($result == false) {
-                return;
-            }
-        }
+		$actionName .= 'Action';
+		if (method_exists($this, $actionName)) {
+			$this->$actionName();
+		} else {
+			include 'view/status-pages/404.html';
+		}
 
-        $actionName .= 'Action';
-        if (method_exists($this, $actionName)) {
-            $this->$actionName();
-        } else {
-            include 'view/status-pages/404.html';
-        }
-
-    }
+	}
 }

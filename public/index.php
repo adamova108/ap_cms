0d567b407a8470bb71755a02a18167cf9645855c
Adam L <luzsiadam@gmail.com>
2021-09-29 19:49:43 +0200

    + Updates 6a

--- public/index.php
+++ public/index.php
@@ -2,11 +2,18 @@
 session_start();
 
 define('ROOT_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR);
-const VIEW_PATH = ROOT_PATH . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR;
+define('VIEW_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR);
 
 
-require_once ROOT_PATH . 'src/controller.php';
-require_once ROOT_PATH . 'src/template.php';
+require_once ROOT_PATH . 'src/Controller.php';
+require_once ROOT_PATH . 'src/Template.php';
+require_once ROOT_PATH . 'src/DatabaseConnection.php';
+require_once ROOT_PATH . 'model/Page.php';
+
+
+// Bootstrap
+/* Connect to a MySQL database using driver invocation */
+DatabaseConnection::connect('localhost', 'kwd_phpstorm', 'root', '');
 
 
 // if / else logic 
@@ -17,7 +24,7 @@ $action = $_GET['action'] ?? $_POST['action'] ?? 'default';
 
 if ($section == 'about-us') {
 
-	include ROOT_PATH . 'controller/aboutUsPage.php';
+	include ROOT_PATH . 'controller/AboutUsController.php';
 
 	$aboutController = new AboutUsController();
 	$aboutController->runAction($action);

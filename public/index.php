663a260a487fd624c62966904bb4d38fe9f5e096
Adam L <luzsiadam@gmail.com>
2021-10-01 21:14:15 +0200

    + Updates 7b

--- public/index.php
+++ public/index.php
@@ -8,40 +8,34 @@ define('VIEW_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARAT
 require_once ROOT_PATH . 'src/Controller.php';
 require_once ROOT_PATH . 'src/Template.php';
 require_once ROOT_PATH . 'src/DatabaseConnection.php';
+require_once ROOT_PATH . 'src/Entity.php';
+require_once ROOT_PATH . 'src/Router.php';
 require_once ROOT_PATH . 'model/Page.php';
 
 
 // Bootstrap
 /* Connect to a MySQL database using driver invocation */
-DatabaseConnection::connect('localhost', 'kwd_phpstorm', 'root', '');
+DatabaseConnection::connect('localhost', 'darwin_cms', 'root', '');
 
 
-// if / else logic 
+// Routing
+$action = $_GET['seo_name'] ?? 'home';
 
-$section = $_GET['section'] ?? $_POST['section'] ?? 'home';
-$action = $_GET['action'] ?? $_POST['action'] ?? 'default';
+$dbh = DatabaseConnection::getInstance();
+$dbc = $dbh->getConnection();
 
-if ($section == 'about-us') {
+$router = new Router($dbc);
 
-	include ROOT_PATH . 'controller/AboutUsController.php';
+$router->findBy('pretty_url', $action);
 
-	$aboutController = new AboutUsController();
-	$aboutController->runAction($action);
+$action = $router->action != '' ? $router->action : 'default';
+$moduleName = ucfirst($router->module) . 'Controller';
 
-} else if ($section == 'contact') {
 
-	include ROOT_PATH . 'controller/contactPage.php';
-	$contactController = new ContactController();
-	$contactController->runAction($action);
+if (file_exists(ROOT_PATH . 'controller/' . $moduleName . '.php')) {
 
-} else {
-	include ROOT_PATH . 'controller/homePage.php';
-	$homePageController = new HomePageController();
-	$homePageController->runAction($action);
-
+	include ROOT_PATH . 'controller/' . $moduleName . '.php';
+	$controller = new $moduleName();
+	$controller->setEntityId($router->entity_id);
+	$controller->runAction($action);
 }
-
-
-
-
-

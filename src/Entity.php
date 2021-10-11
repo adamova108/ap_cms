d48589574c3f56d451ded393d097201c9d7e4d81
Adam L <luzsiadam@gmail.com>
2021-10-11 18:56:31 +0200

    + Moved non-public elements

---
+++ src/Entity.php
@@ -0,0 +1,42 @@
+<?php
+
+abstract class Entity
+{
+
+	protected $dbc;
+	protected $tableName;
+	protected $fields;
+
+	// Force Extending class to define this method
+	abstract protected function initFields();
+
+	protected function __construct($dbc, $tableName)
+	{
+		$this->dbc = $dbc;
+		$this->tableName = $tableName;
+		$this->initFields();
+	}
+
+
+	public function findBy($fieldName, $fieldValue)
+	{
+
+
+		$sql = "SELECT * FROM " . $this->tableName . " WHERE " . $fieldName . " = :value";
+		$stmt = $this->dbc->prepare($sql);
+		$stmt->execute(['value' => $fieldValue]);
+		$databaseData = $stmt->fetch();
+//         $stmt->debugDumpParams();
+
+		$this->setValues($databaseData);
+
+	}
+
+	public function setValues($values)
+	{
+
+		foreach ($this->fields as $fieldName) {
+			$this->$fieldName = $values[$fieldName];
+		}
+	}
+}

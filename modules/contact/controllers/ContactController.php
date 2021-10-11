61e06d79f7a181ea49e609a86fbb2cdbb00ef22e
Adam L <luzsiadam@gmail.com>
2021-10-11 18:57:08 +0200

    + Moved non-public elements

---
+++ modules/contact/controllers/ContactController.php
@@ -0,0 +1,74 @@
+<?php
+
+class ContactController extends Controller
+{
+
+	function runBeforeAction()
+	{
+
+		if ($_SESSION['has_submitted_the_form'] ?? 0 == 1) {
+
+
+			$dbh = DatabaseConnection::getInstance();
+			$dbc = $dbh->getConnection();
+
+			$pageObj = new Page($dbc);
+			$pageObj->findById(3);
+			$variables['pageObj'] = $pageObj;
+
+			$template = new Template('default');
+			$template->view('page/views/static-page', $variables);
+
+			return false;
+		}
+		return true;
+	}
+
+	function defaultAction()
+	{
+
+		$variables['title'] = '';
+		$variables['content'] = '';
+
+
+		$dbh = DatabaseConnection::getInstance();
+		$dbc = $dbh->getConnection();
+
+		$pageObj = new Page($dbc);
+		$pageObj->findBy('id', $this->entityId);
+		$variables['pageObj'] = $pageObj;
+
+
+		$template = new Template('default');
+		$template->view('contact/views/contact-us', $variables);
+	}
+
+	function submitContactFormAction()
+	{
+
+
+		// validate
+		// store data
+		// send email
+
+		$_SESSION['has_submitted_the_form'] = 1;
+
+
+		$variables['title'] = '';
+		$variables['content'] = '';
+
+
+		$dbh = DatabaseConnection::getInstance();
+		$dbc = $dbh->getConnection();
+
+		$pageObj = new Page($dbc);
+		$pageObj->findById(4);
+		$variables['pageObj'] = $pageObj;
+
+
+		$template = new Template('default');
+		$template->view('page/views/static-page', $variables);
+
+	}
+
+}

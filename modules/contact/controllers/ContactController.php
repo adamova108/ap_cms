1ba5e5fe3e2b8c56f6f9997c1238e60d2121defd
Adam L <luzsiadam@gmail.com>
2021-10-29 18:49:13 +0200

    + Updates 9b

--- modules/contact/controllers/ContactController.php
+++ modules/contact/controllers/ContactController.php
@@ -1,74 +1,71 @@
 <?php
 
-class ContactController extends Controller
-{
+class ContactController extends Controller {
+    
+    function runBeforeAction(){
+        
+        if($_SESSION['has_submitted_the_form'] ?? 0 == 1){   
+            
+            
+            $dbh = DatabaseConnection::getInstance();
+            $dbc = $dbh->getConnection();
 
-	function runBeforeAction()
-	{
+            $pageObj = new Page($dbc);
+            $pageObj->findById(3);
+            $variables['pageObj'] = $pageObj;
 
-		if ($_SESSION['has_submitted_the_form'] ?? 0 == 1) {
+            $template = new Template('default');
+            $template->view('page/views/static-page', $variables);
+        
+            return false;
+        }
+        return true;
+    }
+    
+    function defaultAction(){
+        
+        $variables['title'] = '';
+        $variables['content'] = '';
+        
+        
+        $dbh = DatabaseConnection::getInstance();
+        $dbc = $dbh->getConnection();
+        
+        $pageObj = new Page($dbc);
+        $pageObj->findBy('id', $this->entityId);
+        $variables['pageObj'] = $pageObj;
+        
+        
+        $template = new Template('default');
+        $template->view('contact/views/contact-us', $variables);
+    }
+    function submitContactFormAction() {
+       
+        
+        
+        // validate
+        // store data
+        // send email
+        
+        $_SESSION['has_submitted_the_form'] = 1;
+        
+        
+        $variables['title'] = '';
+        $variables['content'] = '';
+        
+        
+        $dbh = DatabaseConnection::getInstance();
+        $dbc = $dbh->getConnection();
+        
+        $pageObj = new Page($dbc);
+        $pageObj->findById(4);
+        $variables['pageObj'] = $pageObj;
+        
+        
+        
+        $template = new Template('default');
+        $template->view('page/views/static-page', $variables);
 
-
-			$dbh = DatabaseConnection::getInstance();
-			$dbc = $dbh->getConnection();
-
-			$pageObj = new Page($dbc);
-			$pageObj->findById(3);
-			$variables['pageObj'] = $pageObj;
-
-			$template = new Template('default');
-			$template->view('page/views/static-page', $variables);
-
-			return false;
-		}
-		return true;
-	}
-
-	function defaultAction()
-	{
-
-		$variables['title'] = '';
-		$variables['content'] = '';
-
-
-		$dbh = DatabaseConnection::getInstance();
-		$dbc = $dbh->getConnection();
-
-		$pageObj = new Page($dbc);
-		$pageObj->findBy('id', $this->entityId);
-		$variables['pageObj'] = $pageObj;
-
-
-		$template = new Template('default');
-		$template->view('contact/views/contact-us', $variables);
-	}
-
-	function submitContactFormAction()
-	{
-
-
-		// validate
-		// store data
-		// send email
-
-		$_SESSION['has_submitted_the_form'] = 1;
-
-
-		$variables['title'] = '';
-		$variables['content'] = '';
-
-
-		$dbh = DatabaseConnection::getInstance();
-		$dbc = $dbh->getConnection();
-
-		$pageObj = new Page($dbc);
-		$pageObj->findById(4);
-		$variables['pageObj'] = $pageObj;
-
-
-		$template = new Template('default');
-		$template->view('page/views/static-page', $variables);
-
-	}
-
+    }
+    
 }

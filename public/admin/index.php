91bf6c69b58f8edf83c76472085a6092e3dbe19f
Adam Luzsi <luzsiadam@gmail.com>
2022-03-02 18:12:43 +0100

    + Index & template updates

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,12 +1,13 @@
 <?php
 session_start();
 
-define('ROOT_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
+define('ROOT_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR );
 define('VIEW_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR);
 define('MODULE_PATH', ROOT_PATH . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR);
 
 define('ENCRYPTION_SALT', 'jh3245hgdfv8934hu3nvr4h5i');
 
+// TODO: move to autoInclude
 
 require_once ROOT_PATH . 'src/Controller.php';
 require_once ROOT_PATH . 'src/Template.php';
@@ -14,8 +15,14 @@ require_once ROOT_PATH . 'src/DatabaseConnection.php';
 require_once ROOT_PATH . 'src/Entity.php';
 require_once ROOT_PATH . 'src/Router.php';
 require_once ROOT_PATH . 'src/Auth.php';
+require_once ROOT_PATH . 'src/Validation.php';
+require_once ROOT_PATH . 'src/validationRules/ValidateMinimum.php';
+require_once ROOT_PATH . 'src/validationRules/ValidateMaximum.php';
+require_once ROOT_PATH . 'src/validationRules/ValidateEmail.php';
+require_once ROOT_PATH . 'src/validationRules/ValidateSpecialCharacter.php';
 require_once MODULE_PATH . 'page/models/Page.php';
 require_once MODULE_PATH . 'user/models/User.php';
+
 
 
 // Bootstrap
@@ -23,17 +30,19 @@ require_once MODULE_PATH . 'user/models/User.php';
 DatabaseConnection::connect('localhost', 'darwin_cms', 'root', '');
 
 
-// if / else logic
+
+// if / else logic 
 
 $module = $_GET['module'] ?? $_POST['module'] ?? 'dashboard';
 $action = $_GET['action'] ?? $_POST['action'] ?? 'default';
 
 
-if ($module == 'dashboard') {
 
-	include MODULE_PATH . 'dashboard/admin/controllers/DashboardController.php';
-
-	$dashboardController = new DashboardController();
-	$dashboardController->runAction($action);
-
+if ($module=='dashboard') {
+    
+    include MODULE_PATH . 'dashboard/admin/controllers/DashboardController.php';
+    
+    $dashboardController = new DashboardController();
+    $dashboardController->runAction($action);
+    
 }
--- src/template.php
+++ src/template.php
@@ -1,18 +1,15 @@
 <?php
 
-class Template
-{
-	private $layout;
-
-	public function __construct($layout)
-	{
-		$this->layout = $layout;
-	}
+class Template {
+    private $layout;
+    
+    public function __construct($layout) {
+        $this->layout = $layout;
+    }
 
 
-	function view($template, $variables)
-	{
-		extract($variables);
-		include VIEW_PATH . 'layout/' . $this->layout . '.html';
-	}
+    function view($template, $variables){
+        extract($variables);
+        include VIEW_PATH . 'layout/' . $this->layout .  '.html';  
+    }
 }

d013d433c7cadd60bb7ee38168020bd9469d2bc2
Adam Luzsi <luzsiadam@gmail.com>
2022-03-02 18:13:17 +0100

    + Updated controller

--- src/Controller.php
+++ src/Controller.php
@@ -1,30 +1,27 @@
 <?php
 
-class Controller
-{
+class Controller {
+    
+    protected $entityId;
 
-	protected $entityId;
-
-	public function runAction($actionName)
-	{
-		if (method_exists($this, 'runBeforeAction')) {
-			$result = $this->runBeforeAction();
-			if ($result == false) {
-				return;
-			}
-		}
-
-		$actionName .= 'Action';
-		if (method_exists($this, $actionName)) {
-			$this->$actionName();
-		} else {
-			include 'view/status-pages/404.html';
-		}
-
-	}
-
-	public function setEntityId($entityId)
-	{
-		$this->entityId = $entityId;
-	}
+    public function runAction($actionName) {
+        if(method_exists($this, 'runBeforeAction')){
+            $result = $this->runBeforeAction();
+            if($result == false) {
+                return;
+            }
+        }
+        
+        $actionName .= 'Action';
+        if (method_exists($this, $actionName)){
+            $this->$actionName();
+        } else {
+            include 'view/status-pages/404.html';
+        }
+        
+    }
+    
+    public function setEntityId($entityId) {
+        $this->entityId = $entityId;
+    }
 }

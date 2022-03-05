abdd650c454d28ed279884fc29a227bda1964ac4
Adam Luzsi <luzsiadam@gmail.com>
2022-03-05 11:08:17 +0100

    + Added DashboardController

--- modules/dashboard/admin/controllers/DashboardController.php
+++ modules/dashboard/admin/controllers/DashboardController.php
@@ -1,48 +1,69 @@
 <?php
 
-class DashboardController extends Controller
-{
+class DashboardController extends Controller {
+    
+    function runBeforeAction() {
+        if($_SESSION['is_admin'] ?? false == true){
+            return true;
+        }
+        $action = $_GET['action'] ?? $_POST['action'] ?? 'default';
+        if($action != 'login'){
+            header('Location: /admin/index.php?module=dashboard&action=login');
+        } else {
+            return true;
+        }
+    }
 
-	function runBeforeAction()
-	{
-		if ($_SESSION['is_admin'] ?? false == true) {
-			return true;
-		}
-		$action = $_GET['action'] ?? $_POST['action'] ?? 'default';
-		if ($action != 'login') {
-			header('Location: /admin/index.php?module=dashboard&action=login');
-		} else {
-			return true;
-		}
-	}
+    function defaultAction() {
+        
+        echo "Welcome to the administration";
+        
+    }
+    
+    function loginAction() {
+        
+        if($_POST['postAction'] ?? 0 == 1){
+            $username = $_POST['username'] ?? '';
+            $password = $_POST['password'] ?? '';
+            
+            $validation = new Validation();
+            if(!$validation
+                    ->addRule(new ValidateMinimum(3))
+                    ->addRule(new ValidateMaximum(20))
+                    ->addRule(new ValidateSpecialCharacter())
+                    ->validate($password)
+                    ) {
+                
+                $_SESSION['validationRules']['error'] = "Password must be between 3 and 20 characters"
+                        . " and must contain one special character";
+            }
+            
+            if(!$validation
+                    ->addRule(new ValidateMinimum(3))
+                    ->addRule(new ValidateEmail())
+                    ->validate($username)
+                    ) {
+                $_SESSION['validationRules']['error'] = "Username is not valid email";
+            }
+            
+            if(($_SESSION['validationRules']['error'] ?? '') == ''){
+                $auth = new Auth();
+                if($auth->checkLogin($username, $password)){
+                   // all is good
+                    $_SESSION['is_admin'] = 1;
+                    header('Location: /admin/');
+                    exit();
+                }
 
-	function defaultAction()
-	{
+                $_SESSION['validationRules']['error'] = "Username or password is incorect";
+            }
 
-		echo "Welcome to the administration";
-
-	}
-
-	function loginAction()
-	{
-
-		if ($_POST['postAction'] ?? 0 == 1) {
-			$username = $_POST['username'] ?? '';
-			$password = $_POST['password'] ?? '';
-
-			$auth = new Auth();
-			if ($auth->checkLogin($username, $password)) {
-				// all is good
-				$_SESSION['is_admin'] = 1;
-				header('Location: /admin/');
-				exit();
-			}
-
-			var_dump('bad login');
-		}
-
-
-		include VIEW_PATH . 'admin/login.html';
-	}
+        }
+        
+        
+        
+        include VIEW_PATH . 'admin/login.html';
+        unset($_SESSION['validationRules']['error']);
+    }
 
 }

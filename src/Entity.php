23a8cb35cc7111a777bdc689d312a2b65d4d751e
Adam Luzsi <luzsiadam@gmail.com>
2022-03-09 16:44:01 +0100

    + Updated Entity

--- src/Entity.php
+++ src/Entity.php
@@ -1,43 +1,39 @@
 <?php
 
-abstract class Entity
-{
+abstract class Entity {
+    
+    protected $dbc;
+    protected $tableName;
+    protected $fields;
+    
+    // Force Extending class to define this method
+    abstract protected function initFields();
+    
+    protected function __construct($dbc, $tableName){
+        $this->dbc = $dbc;
+        $this->tableName = $tableName;
+        $this->initFields();
+    }
+    
+    
+    public function findBy($fieldName, $fieldValue){
+        
 
-	protected $dbc;
-	protected $tableName;
-	protected $fields;
-
-	// Force Extending class to define this method
-	abstract protected function initFields();
-
-	protected function __construct($dbc, $tableName)
-	{
-		$this->dbc = $dbc;
-		$this->tableName = $tableName;
-		$this->initFields();
-	}
-
-
-	public function findBy($fieldName, $fieldValue)
-	{
-
-
-		$sql = "SELECT * FROM " . $this->tableName . " WHERE " . $fieldName . " = :value";
-		$stmt = $this->dbc->prepare($sql);
-		$stmt->execute(['value' => $fieldValue]);
-		$databaseData = $stmt->fetch();
+        $sql = "SELECT * FROM " . $this->tableName . " WHERE " . $fieldName . " = :value";
+        $stmt = $this->dbc->prepare($sql);
+        $stmt->execute(['value'=> $fieldValue]);
+        $databaseData = $stmt->fetch();
 //         $stmt->debugDumpParams();
-		if ($databaseData) {
-			$this->setValues($databaseData);
-		}
+        if($databaseData){
+            $this->setValues($databaseData);
+        }
 
-	}
-
-	public function setValues($values)
-	{
-
-		foreach ($this->fields as $fieldName) {
-			$this->$fieldName = $values[$fieldName];
-		}
-	}
+    }
+    
+    public function setValues($values) {
+        
+        foreach ($this->fields as $fieldName) {
+            $this->$fieldName = $values[$fieldName];
+        }
+    }
 }
